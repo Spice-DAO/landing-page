50eab5c894ad44bb3eb02959ef5d321366abb0a9
0xNotes <notes@sigil.systems>
2022-04-12 23:15:43 -0500

    Navgroup cleaned

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -29,18 +29,18 @@ const NavBar = () => {
         <div className='NavContainer'>
             <img className='Nav__logo' src={logo} alt="Spice DAO logo" />
             <div className='NavContainer__icon_group'>
-            <NavLink to="/"><FaHome size={37} className='NavContainer__icons' /></NavLink>
-            <span>Home</span>
+            <NavLink to="/"><FaHome size={30} className='NavContainer__icons' /></NavLink>
+            Home
             </div>
             <div className='NavContainer__icon_group'>
-            <NavLink to="/shop"><FaShoppingBag size={37} className='NavContainer__icons' /></NavLink>
-            <span>Shop</span>
+            <NavLink to="/shop"><FaShoppingBag size={30} className='NavContainer__icons' /></NavLink>
+            Shop
             </div>
             <div className='NavContainer__icon_group'>
-            <NavLink to="/cart"><FaShoppingCart size={37} className='NavContainer__icons' /></NavLink>
+            <NavLink to="/cart"><FaShoppingCart size={30} className='NavContainer__icons' /></NavLink>
                 {/* DO AN INTERPOLATION BELOW TO GET THE NUMBER OF ITEMS */}
                 {/* Ternary for cart parenthesees */}
-            <span>Cart</span>
+            Cart
             </div>
         </div>
 
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -4,7 +4,6 @@ import NavBar from "../components/NavBar"
 import { Card } from "react-bootstrap";
 import hoodie from '../../src/hoodie.png';
 import tshirt from '../../src/tshirtresize.png';
-import Button from "react-bootstrap/Button";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 

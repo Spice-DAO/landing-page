c451c071f5950f895f9d128c689624f1ffec1fa9
0xNotes <notes@sigil.systems>
2022-04-15 13:16:40 -0500

    Added Custom Inputs, Input Validation, Order Submission Notification

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,6 +7,8 @@ import NavBar from "../components/NavBar"
 import { NavLink } from 'react-router-dom';
 import { Card } from "react-bootstrap";
 import CheckoutItem from '../components/CheckoutItem';
+import CustomInput from '../components/CustomInput';
+
 import hoodie from '../../src/hoodie.png';
 import tshirt from '../../src/tshirtresize.png';
 
@@ -16,7 +18,58 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+
 const Checkout = (props) => {
+  const [inputs, setInputs] = useState({});
+  const [msg, setMsg] = useState('Checkout');
+
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log("CHANGED::", name, value);
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  function handleSubmit() {
+    console.log(inputs.fullname);
+    console.log(inputs.txn);
+    console.log(inputs.address);
+    console.log(inputs.postalCode);
+    console.log(inputs.city);
+    console.log(inputs.country);
+    console.log(inputs.twitter);
+    console.log(inputs.discord);
+    if (inputs.fullname === "" ||
+      inputs.fullname === undefined ||
+      inputs.txn === "" ||
+      inputs.txn === undefined ||
+
+      inputs.address === "" ||
+      inputs.address === undefined ||
+
+      inputs.postalCode === "" ||
+      inputs.postalCode === undefined ||
+
+      inputs.city === "" ||
+      inputs.city === undefined ||
+
+      inputs.country === "" ||
+      inputs.country === undefined
+      ) {
+      alert("Please Fill Out Required Fields and Resubmit")
+    } else {
+      setMsg("Order Submittied")
+    }
+
+  }
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   alert(inputs);
+  // }
+
+
 
   //This can be used to store the items that they have requested.
   //Can be kinda ugly and just track the shirt and hoodie numbers rather than any object orients stuff 
@@ -31,9 +84,35 @@ const Checkout = (props) => {
       <div className='CheckoutInputContainer'>
         <div></div><NavLink to="/shop">&lt; Back To cart</NavLink><div>
           <Card><div>Thanks For Your Purchase</div></Card>
-          <div>
-            <form><input type="text"></input></form>
+
+          <div>Checkout</div>
+          <form>
+            <div className='CheckoutRowFlex'>
+              <CustomInput label="Name" name="fullname" input={inputs.fullname} override="" change={handleChange} />
+              <CustomInput label="Transaction Number" name="txn" input={inputs.txn} override="" change={handleChange} />
+              {/* <form><input className='CleanForm' type="text"></input></form> */}
+            </div>
+            <div>
+              <CustomInput label="Address (Number and street name)" name="address" input={inputs.address} override="" change={handleChange} />
+            </div>
+            <div className='CheckoutRowFlex'>
+              <CustomInput label="Postal Code" name="postalCode" input={inputs.postalCode} override="" change={handleChange} />
+              <CustomInput label="City" name="city" input={inputs.city} override="" change={handleChange} />
+              <CustomInput label="Country" name="country" input={inputs.country} override="" change={handleChange} />
+            </div>
+            <div className='CheckoutRowFlex'>
+              <CustomInput label="Twitter" name="twitter" input={inputs.twitter} override="@" change={handleChange} />
+              <CustomInput label="Discord" name="discord" input={inputs.discord} override="" change={handleChange} />
+            </div>
+          </form>
+
+          {/*         <a href='#0' onClick={() => setMsg('Coming Soon')} >{msg}</a>
+ */}
+          <div className="Main__links">
+            <a href='#0' style={{ width: "100%" }} onClick={() => handleSubmit()}>{msg}</a>
           </div>
+
+
         </div>
       </div>
 
---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const CustomInput = (props) => {
+
+    // Largely going to mirror cards in construction
+
+    return (
+        <div className='CustomInputContainer'>
+
+            {/* Needs to provide a flex with a Label above it and an Input Below it */}
+            <div>{props.label}</div>
+            {/* Needs to hook to a useState */}
+                <input className='CleanForm' name={props.name} value={props.input || props.override} onChange={props.change} type="text"></input>
+        </div>
+    )
+}
+
+export default CustomInput

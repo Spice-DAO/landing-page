c595897ad313efe1632a12c48528abdef95e4660
0xNotes <notes@sigil.systems>
2022-04-27 15:44:08 -0500

    Fixed link location

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -33,7 +33,7 @@ const Home = () => {
       <div className="Main__social_icons">
       <a href="https://twitter.com/TheSpiceDao"> { <FontAwesomeIcon icon={ faTwitter } /> } </a>
       <a href="http://discord.gg/SPICEDAO">{ <FontAwesomeIcon icon={ faDiscord } /> }</a>
-      <a href="https://medium.com/@TheSpiceDao">{ <FontAwesomeIcon icon={ faMedium } /> }</a>
+      <a href="https://medium.com/@SpiceDao">{ <FontAwesomeIcon icon={ faMedium } /> }</a>
       <a href="mailto:team@spicedao.xyz">< FaEnvelope/></a>
     </div>
     {/* <Shop /> */}

e1c732aa896bc4137a72c05f70df35e0c7c6d48e
0xNotes <notes@sigil.systems>
2022-06-27 18:40:01 -0500

    Ternaryception solution to error messages

--- src/components/StateButton.jsx
+++ src/components/StateButton.jsx
@@ -11,21 +11,24 @@ import React, { useState } from 'react';
 export default function StateButton(props) {
     const [msg, setMsg] = useState(props.buttonText);
     const [altMsg, setAltMsg] = useState(props.buttonText);
+    const [error, setError] = useState(false);
  
     // {props.walletConnected ? (<div className="TransitionButton" onClick={() => {props.checkSpiceHandler("tashka");}}>{props.buttonText}</div>) : (<div className="TransitionButton" onMouseOver={() => setTashkaMsg("Connect Wallet")}  onClick={() => {props.connectWalletHandler();}}> {tashkaMsg}</div>)}
     
     function validate(){
         if(!props.spiceFound){
+            setError(true);
             setAltMsg("No $SPICE Found")
         }
     }
 
 
 
+            
     return(
     <div>
     {props.walletConnected ? 
-    (<div className="TransitionButton" onMouseOver={() => {setAltMsg(props.action);}} onMouseLeave={() => {setAltMsg(props.buttonText);}} onClick={() => {props.checkSpiceHandler(props.flagHandler);}}>{altMsg}</div>) 
+    (<div className="TransitionButton" onMouseOver={() => {error? (setAltMsg("No $SPICE Found")) : (setAltMsg(props.action))}} onMouseLeave={() => {setAltMsg(props.buttonText);}} onClick={() => {props.checkSpiceHandler(props.flagHandler).then(validate)}}>{altMsg}</div>)
     : 
     (<div className="TransitionButton" onMouseOver={() => setMsg("Connect Wallet")} onMouseLeave={() => setMsg(props.buttonText)} onClick={() => {props.connectWalletHandler();}}> {msg}</div>)}
     </div>
